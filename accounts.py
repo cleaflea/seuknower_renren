7ed4bcc08e878cd7b3d60d9d057a3dc5128eba3a
cleantha <cleantha@ubuntu.(none)>
2013-12-27 21:07:22 +0800

    delete some accounts

--- accounts.py
+++ accounts.py
@@ -2,7 +2,5 @@
 
 # 账号列表，一行一个
 accounts = [
-    ('441035424@qq.com', 'z1x2c3v41598753')
-    # ('zjhsdtc@sohu.com', 'z1x2c3v41598753')
-    # ('1652448220@qq.com', 'dongdatong')
+    # ('usernamehere', 'passwordhere')
 ]
